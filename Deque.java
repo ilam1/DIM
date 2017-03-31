ca4a20bf237dafd9f2f1094f3144ca6de86d0ccd
ilam1 <ilam1@stuy.edu>
2017-03-31 11:46:20 -0400

    Descriptions

--- Deque.java
+++ Deque.java
@@ -1,19 +1,29 @@
 public interface Deque<T> {
-    public boolean add(T e);
 
+    /**Inserts parameter e of type T to the end of the deque
+     * Precondition: Parameter e is added
+     * Postcondition: e is added to the end of deque */
+    public void addLast(T e);
+
+    /**Inserts parameter e of type T to the beginning of the deque
+     * Precondition: Parameter e is added
+     * Postcondition: e is added to the beginning of deque */
     public void addFirst(T e);
 
+    /**Retrieves the first element of deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the first element of deque if !isEmpty, otherwise returns null */
     public T peekFirst();
 
+    /**Retrieves the last element of deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the last element of deque if !isEmpty, otherwise returns null */
     public T peekLast();
-    
-    public T peek();
 
+    /**Determines whether parameter e is contained in deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns true if parameter e is in deque, false otherwise*/
     public boolean contains(Object o);
-    
-    public T getFirst();
-    
-    public T getLast();
 
     public T remove();
 
