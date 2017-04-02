395c0dc1eedcbc7357c04806b3244e7ae09cb61b
ilam1 <ilam1.stuy.edu>
2017-04-02 12:19:56 -0400

    Added corresponding messages

--- Deque.java
+++ Deque.java
@@ -1,29 +1,52 @@
 public interface Deque<T> {
-    public boolean add(T e);
+    /**Inserts parameter e of type T to the end of the deque
+     * Precondition: Parameter e is added
+     * Postcondition: e is added to the end of deque */
+    public void addLast(T e);
 
+    /**Inserts parameter e of type T to the beginning of the deque
+     * Precondition: Parameter e is added
+     * Postcondition: e is added to the beginning of deque */
     public void addFirst(T e);
 
+    /**Retrieves the first element of deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the first element of deque if !isEmpty, otherwise returns null */
     public T peekFirst();
 
+    /**Retrieves the last element of deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the last element of deque if !isEmpty, otherwise returns null */
     public T peekLast();
-    
-    public T peek();
 
+    /**Determines whether parameter e is contained in deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns true if parameter e is in deque, false otherwise*/
     public boolean contains(Object o);
-    
-    public T getFirst();
-    
-    public T getLast();
 
+    /**Removes the last element of deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the last element of deque if !isEmpty, otherwise returns null*/
     public T remove();
 
+    /**Removes the first element of  deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the first element of deque if !isEmpty, otherwise returns null*/
     public T removeFirst();
 
-    public T removeLast();
-
+    /**Removes the first occurrence of parameter o
+     * Precondition: Parameter o exists in deque
+     * Postcondition: Returns true if parameter o is removed from deque*/
     public boolean remove(Object o);
 
+    /**Determines the size of deque
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the size of deque*/
     public int size();
 
+    /**Print method
+     * Precondition: Deque is initialized
+     * Postcondition: Returns the string version of deque*/
     public String toString();
 }
+
