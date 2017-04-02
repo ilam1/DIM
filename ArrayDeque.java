2a0306973c1ffa975464000fc933118fcafd51ad
ilam1 <ilam1@stuy.edu>
2017-04-02 12:29:42 -0400

    Class to implement interface

---
+++ ArrayDeque.java
@@ -0,0 +1,7 @@
+
+/*****************************************************
+ * class ArrayDeque
+ * Implements Deque as specified in local Deque.java
+ * Generically typed
+ * Uses an ArrayList as underlying container
+ *****************************************************/
